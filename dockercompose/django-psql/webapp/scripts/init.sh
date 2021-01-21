ef5d96630e7e5ddeefdf4545bb58ab72c9425f4d
LeeHemingway <coherence0815@gmail.com>
2021-01-21 13:33:07 +0900

    refine init.sh for k8s

--- dockercompose/django-psql/webapp/scripts/init.sh
+++ dockercompose/django-psql/webapp/scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until PGPASSWORD=pass psql -h ${POSTGRES_HOST} -p 5432 -U postgres -c "\q"; do
+until PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -p 5432 -U postgres -c "\q"; do
   >&2 echo "Postgres is not available, sleep..."
   sleep 1
 done
